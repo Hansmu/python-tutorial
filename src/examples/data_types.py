02508c6eee610a09551d1f14ee8b0905c28e54d1
hansmu <hans.miikael@gmail.com>
2019-02-10 13:58:51 +0200

    Add string and number examples

---
+++ src/examples/data_types.py
@@ -0,0 +1,48 @@
+def data_types_examples():
+    numbers_and_maths_examples()
+    string_examples()
+
+
+def numbers_and_maths_examples():
+    number = 187213
+    floating_point = 3.1241124
+
+    adding = 2 + 1
+    subtraction = 4 - 1
+    multiplication = 2 * 3
+    division = 5 / 2
+    modulus = 7 % 4
+    power_to = 2 ** 4
+
+    print('-----------------NUMBERS EXAMPLES HERE--------------------')
+    print(number, floating_point, adding, subtraction, multiplication, division, modulus, power_to)
+    print(type(number), type(floating_point))
+
+
+def string_examples():
+    string = 'Hello'
+    also_string = "Hello \nfriend"
+    nested = "Oh no, don't do that!"
+
+    print('--------------STRING EXAMPLES HERE------------------')
+    print(string, also_string, nested)
+    print('Grab from the start: ', string[0], string[1])
+    print('Grab from the end: ', string[-1], string[-2])
+    print('Get the string length: ', len(also_string))
+    print('From index to end: ', also_string[2:])
+    print('Get up until index: ', nested[:3])
+    print('String section: ', nested[2:6])
+    print('Specify step size: ', nested[::2], nested[2:7:2])
+    print('Reverse string: ', string[::-1])
+    print('Concat: ', string + ' ' + also_string)
+    print('Spam letters: ', 'z' * 10)
+    print('Some methods', string.upper(), string.lower(), nested.split(', '))
+
+    print('String here {}, then also {}'.format('yup', 'here too'))
+    print('The {2} {1} {0}'.format('fox', 'brown', 'quick'))
+    print('The {q} {b} {f}'.format(f='fox', b='brown', q='quick'))
+    print('The result was formatted to 3 float points {r:1.3f}'.format(r=100/77))
+    print('The result was formatted to 5 float points {r:1.5f}'.format(r=100/77))
+
+    # f-string
+    print(f'Also interpolation for {100/77}')
