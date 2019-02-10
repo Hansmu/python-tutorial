82e229fac87bb9d65b0372d4f82d70a759c6d9ae
hansmu <hans.miikael@gmail.com>
2019-02-10 15:57:40 +0200

    Add for loop example with tuple unpacking

--- src/examples/operations.py
+++ src/examples/operations.py
@@ -1,6 +1,7 @@
 def operation_examples():
     comparison_examples()
     if_else_examples()
+    loops_examples()
 
 
 def comparison_examples():
@@ -23,3 +24,22 @@ def if_else_examples():
         print('Else if')
     else:
         print('Not gonna reach here with those conditions')
+
+
+def loops_examples():
+    some_list = [1, 2, 3]
+
+    # If the list items aren't used, then _ is often used as the item name
+    for item in some_list:
+        print(item)
+
+    another_list = [(1, 2), (3, 4), (5, 6)]
+
+    # Can add more variables to unpack longer tuples
+    for first_tuple_element, second_tuple_element in another_list:
+        print(first_tuple_element, second_tuple_element)
+
+    dictionary = {'k1': 1, 'k2': 2}
+
+    for key, value in dictionary.items():
+        print(key, value)
