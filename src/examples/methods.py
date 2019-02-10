48d46109741174bfa179eb6c3658adbcd9e8f2f1
hansmu <hans.miikael@gmail.com>
2019-02-10 18:55:27 +0200

    Add lambda examples

--- src/examples/methods.py
+++ src/examples/methods.py
@@ -19,3 +19,13 @@ def name_method_example(name='Bobbert'):
 # myFunc(10, 20, 30, fruit='orange', food='eggs', animal='dog')
 def arbitrary_number_of_inputs_example(*args):
     print(sum(args) * 0.05)
+
+
+def lambda_examples():
+    my_nums = [1, 2, 3, 4, 5]
+    # Or provide a function in place as a reference
+    squared_nums = list(map(lambda x: x**2, my_nums))
+    even_squared = list(filter(lambda num: num % 2 == 0, squared_nums))
+    print(squared_nums)
+    print(even_squared)
+
