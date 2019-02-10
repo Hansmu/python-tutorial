cecb82557cb0372c8563da1f38e65ba9e6ce5642
hansmu <hans.miikael@gmail.com>
2019-02-10 17:06:01 +0200

    Add method documentation and default parameter examples

---
+++ src/examples/methods.py
@@ -0,0 +1,16 @@
+def method_example():
+    '''
+    DOCSTRING: Information about the method is added here.
+    INPUT: information about the input variables
+    OUTPUT: information about the output
+    '''
+
+    name_method_example('Sally')
+    name_method_example()
+
+
+def name_method_example(name='Bobbert'):
+    print('Hello ' + name)
+    return name
+
+
