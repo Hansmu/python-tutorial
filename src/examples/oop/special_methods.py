9d33816cf68a5d85ac0ae9ea83f9a259068cef68
hansmu <hans.miikael@gmail.com>
2019-02-11 12:32:36 +0200

    Add examples of implementing some default methods on classes

---
+++ src/examples/oop/special_methods.py
@@ -0,0 +1,20 @@
+class Book:
+    def __init__(self, title, pages):
+        self.__title__ = title
+        self.__pages__ = pages
+
+    def __str__(self):
+        return "The title is " + self.__title__
+
+    def __len__(self):
+        return self.__pages__
+
+    def __del__(self):
+        print('Book was removed')
+
+
+book = Book('Title', 200)
+
+print(book)
+print(len(book))
+del book
